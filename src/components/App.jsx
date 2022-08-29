1ef4bf104b2573ae03aa89e2dcdc7d8f73f45496
Yotis56 - home <ingyotis@gmail.com>
2022-08-28 21:21:17 -0500

    Recovery password page added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Layout } from "../containers/Layout";
 import { Login } from "../containers/Login";
+import { RecoveryPassword } from "../containers/RecoveryPassword";
 import '../styles/_global.scss'
 
 const App = () => {
     return (
         <Layout>
             <Login />
+            <RecoveryPassword />
         </Layout>
     )
 }
