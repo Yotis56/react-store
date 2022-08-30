ab7d586641dc8887712a4c364fef343f8666edf9
Yotis56 - home <ingyotis@gmail.com>
2022-08-29 21:48:50 -0500

    Not found page added

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import { Link } from 'react-router-dom'
+import '../styles/notFound.scss'
+import notFoundImage from '../assets/notFound.svg'
 
 const NotFound = () => {
     return (
-        <div>
-            No encontrado.
+        <div className="notFound">
+            <div className="img-container">
+                <img src={notFoundImage} alt="" />
+            </div>
+            <div className="text-container">
+                <h1 className="notF-title">404</h1>
+                <h2 className="notF-subtitle">¡OH! Parece que estás perdido</h2>
+                <p>La página que buscas no existe. Sin embargo, puedes dar click en el botón de abajo para volver a la página de inicio</p>
+                <button className="notF-return-button btn">
+                    <Link to="/">Home</Link>
+                </button>
+            </div>
         </div>
     )
 }
